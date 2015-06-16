5fb23e23a219fda8fa3f0d9fb3466bef5e992222
Ty <ty.clark@bayefficiency.com>
2015-06-16 00:19:38 -0700

    adding images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,17 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+
+//= require stampjs/jquery.min.js
+//= require stampjs/smoothscroll.js
+//= require stampjs/bootstrap.min.js
+//= require stampjs/jquery.nav.js
+//= require stampjs/wow.min.js
+//= require stampjs/nivo-lightbox.min.js
+//= require stampjs/owl.carousel.min.js
+//= require stampjs/jquery.stellar.min.js
+//= require stampjs/jquery.ajaxchimp.min.js
+//= require stampjs/custom-1.01.js
+
 //= require_tree .
+
