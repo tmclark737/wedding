8f4fa99d9d40e55efc398c1296beda0e80913299
Ty <ty.clark@bayefficiency.com>
2015-07-21 10:03:14 -0700

    finishing basic rsvp functionality

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -5,10 +5,19 @@ def index
 end
 
 def edit
-	@family = Family.find(params[:id])
+	family = Family.find(params[:id])
+	@guests = family.guests
 end
 
 def show
 end
+
+def update
+
+	Guest.where(id: params[:guest_ids]).update_all(response: true)
+	redirect_to family_path(params[:id])
+
+end
+
 
 end
