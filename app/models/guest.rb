af8b2c7527e97631a7319d7cabca9bf7ab9640c2
Ty <ty.clark@bayefficiency.com>
2015-08-04 20:27:14 -0700

    adding validations

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,7 @@
 class Guest < ActiveRecord::Base
   belongs_to :family
 
-  #validates :response, presence: true
+  validates :response, presence: true
 
 def self.search(search)
 	where('full_name ILIKE ?', "%#{search}%")
